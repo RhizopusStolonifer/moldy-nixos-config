97110a51b968de9e6b14e17640d27214aa5bff4a
RhizopusStolonifer <raskpaul@pm.me>
2026-08-20 14:39:42 -0700

    initial setup for fw16

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Moldy NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, disko, ... }@inputs: {
+    nixosConfigurations.fw16 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        .hosts/fw16
+        disko.nixosModules.disko
+      ];
+    };
+  };
+}
---
+++ hosts/fw16/default.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./disko.__shell.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "fw16";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Vancouver";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
+  users.users.myco = {
+    inNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    initialPassword = "changeme";
+  };
+
+  environment.systemPackages = with pkgs; [git vim];
+  services.openssh.enable = true;
+
+  system.stateVersion = "26.05";
+}
