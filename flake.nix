75797732899fcbf9290031fdb8567fc175d7cd07
RhizopusStolonifer <raskpaul@pm.me>
2026-08-21 10:42:28 -0700

    added home-manager stuff to flake.nix

--- flake.nix
+++ flake.nix
@@ -5,15 +5,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-mananger";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, disko, ... }@inputs: {
+  outputs = { self, nixpkgs, disko, home-manager, ... }@inputs: {
     nixosConfigurations.fw16 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         hosts/fw16
         disko.nixosModules.disko
+        home-manager.nixosModules.default
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.myco = import ../../home/myco
+        }
       ];
     };
   };
