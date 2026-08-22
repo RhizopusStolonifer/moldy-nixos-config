7d2a28cb9f362db7ab446711f9a40d1fd7244949
Moldy Bread <raskpaul@pm.me>
2026-08-22 22:23:26 +0000

    fuckin what

--- flake.nix
+++ flake.nix
@@ -25,6 +25,9 @@
       fw12 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
+        nixpkgs.overlays = [
+          {final: prev: { libdisplay-info_0_2 = prev.libdisplay-info; }}
+        ];
         modules = [
           ./hosts/fw12
           disko.nixosModules.disko
