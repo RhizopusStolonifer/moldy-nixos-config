ea69e9a100a0a090b3491d701e94cc21827d1c5e
Rhizopus Stolonifer <codeberg.pension282@passmail.net>
2026-09-02 12:13:50 -0700

    yerp

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -4,15 +4,26 @@
     enable = true;
 
     autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
+    historySubstringSearch.enable = true;
+    fastsyntaxHighlighting.enable = true;
 
-    history = {
-      share = true;
-      ignoreSpace = true;
-      ignoreDups = true;
-      saveNoDups = true;
-      findNoDups = true;
-      expireDuplicatesFirst = true;
+    shellAliases = {
+      cat = "echo use bat";
+      eza = "eza -lah";
+    };
+
+    zsh-abbr = {
+      enable = true;
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "colored-man-pages"
+        "extract"
+        "eza"
+      ];
     };
 
     plugins = [
@@ -26,15 +37,41 @@
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
       {
+        name = "zsh-completions";
+        src = pkgs.zsh-completions
+      }
+      {
         name = "zsh-autopair";
         src = "${pkgs.zsh-autopair}/share/zsh/zsh-autopair";
         file = "autopair.zsh";
       }
     ];
+
+    initContent = lib.mkMerge [
+      # p10k instant prompt
+      (lib.mkOrder 500 ''
+        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''$(%):-%n}.zsh
+        fi
+      '')
+      (lib.mkOrder 1500 ''
+        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+      '')
+    ];
+
   };
 
-    programs.zoxide = {
+  programs.zoxide = {
       enable = true;
       enableZshIntegration = true;
   };
+
+  home.sessionVariables = {
+    EDITOR = "emacs";
+    VISUAL = "emacs";
+  };
+
+  home.sessionPath = [ "${config.home.homeDirectory}/.config/emacs/bin ];
+
+  home.file.".p10k.zsh".source = ./p10k.zsh;
 }
