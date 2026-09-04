bfae182eba4cda7203d6ec71dc32397751e73bce
Rhizopus Stolonifer <codeberg.pension282@passmail.net>
2026-09-04 16:16:22 -0700

    add zsa udev rules?

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -13,4 +13,13 @@
       swaylock.enableGnomeKeyring = true;
     };
   };
+
+  services.udev.extraRules = ''
+    # ZSA keyboards — normal mode (USB + HID)
+    SUBSYSTEM=="usb", ATTR{idVendor}=="3297", MODE:="0666"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3297", MODE:="0666"
+
+    # STM32 DFU bootloader (generic)
+    SUBSYSTEM=="usb", ATTR{idVendor}=="0483", ATTR{idProduct}=="df11", MODE:="0666"
+  '';
 }
