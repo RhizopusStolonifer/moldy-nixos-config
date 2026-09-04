25a3bd1459233059bb014a4b3941bb399bd84124
Rhizopus Stolonifer <codeberg.pension282@passmail.net>
2026-09-04 11:01:14 -0700

    add niri binds

---
+++ modules/home/niri/default.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  pkgs,
+  host,
+  ...
+}:
+
+{
+  xdg.configFile."niri/config.kdl".source = ./config.kdl;
+  xdg.configFile."niri/dms/binds.kdl".source = ./binds.kdl;
+  xdg.configFile."niri/dms/binds-host.kdl".source =
+    if host == "fw12" then
+      ./binds-mycorrhiza.kdl
+    else if host == "mycorrhiza" then
+      ./binds-fw12.kdl
+    else
+      ./binds.kdl;
+}
