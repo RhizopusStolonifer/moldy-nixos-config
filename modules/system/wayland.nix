c232d02a058a39b1296620430ef6fd0204a25b33
Rhizopus Stolonifer <codeberg.pension282@passmail.net>
2026-09-03 20:10:47 -0700

    add xdg-desktop-portal-gtk

--- modules/system/wayland.nix
+++ modules/system/wayland.nix
@@ -8,6 +8,7 @@
   environment.systemPackages = with pkgs; [ xwayland-satellite ];
   xdg.portal = {
     enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
     config = {
       common.default = [ "gtk" ];
