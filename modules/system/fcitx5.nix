20762a69fc4ec70eaa6409a3e2de0afcaddd871e
Rhizopus Stolonifer <codeberg.pension282@passmail.net>
2026-09-04 20:09:27 -0700

    add fcitx5

--- modules/system/fcitx5.nix
+++ modules/system/fcitx5.nix
@@ -11,18 +11,47 @@
     type = "fcitx5";
     fcitx5 = {
       waylandFrontend = true;
+      ignoreUserConfig = true; # Ignore local changes in ~/.config/fcitx5
+
       addons = with pkgs; [
         fcitx5-mozc
         fcitx5-gtk
         qt6Packages.fcitx5-configtool
       ];
-    };
-  };
 
-  # Set required environment variables for applications to communicate with Fcitx5
-  environment.sessionVariables = {
-    GTK_IM_MODULE = "fcitx";
-    QT_IM_MODULE = "fcitx";
-    XMODIFIERS = "@im=fcitx";
+      settings = {
+        # Configures ~/.config/fcitx5/profile or /etc/xdg/fcitx5/profile
+        inputMethod = {
+          "GroupOrder" = {
+            "0" = "Default";
+          };
+          "Groups/0" = {
+            "Name" = "Default";
+            "Default Layout" = "us";
+            "DefaultIM" = "keyboard-us";
+          };
+          "Groups/0/Items/0" = {
+            "Name" = "keyboard-us";
+            "Layout" = "";
+          };
+          "Groups/0/Items/1" = {
+            "Name" = "mozc";
+            "Layout" = "";
+          };
+        };
+
+        # Configures ~/.config/fcitx5/config or /etc/xdg/fcitx5/config
+        globalOptions = {
+          "Hotkey" = {
+            # Keybind to toggle/switch input method layout
+            "TriggerKeys" = "Super+k";
+            "EnumerateForwardKeys" = "Super+k";
+          };
+          "Behavior" = {
+            "ActiveByDefault" = "True";
+          };
+        };
+      };
+    };
   };
 }
