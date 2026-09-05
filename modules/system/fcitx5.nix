91be4a1560228edd53cea9e751296b2a8cc21227
Rhizopus Stolonifer <codeberg.pension282@passmail.net>
2026-09-04 19:54:59 -0700

    add fcitx5

--- modules/system/fcitx5.nix
+++ modules/system/fcitx5.nix
@@ -9,13 +9,20 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      qt6Packages.fcitx5-with-addons
-      fcitx5-mozc
-      qt6Packages.fcitx5-configtool
-    ];
-    fcitx5.waylandFrontend = true;
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+        qt6Packages.fcitx5-configtool
+      ];
+    };
   };
 
-  services.xserver.desktopManager.runXdgAutostartIfNone = true;
+  # Set required environment variables for applications to communicate with Fcitx5
+  environment.sessionVariables = {
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+  };
 }
