01d192425dbf7a15c176040a9b13c75645e16864
Rhizopus Stolonifer <codeberg.pension282@passmail.net>
2026-09-09 04:29:44 -0700

    fix steam issues?

--- modules/system/wayland.nix
+++ modules/system/wayland.nix
@@ -5,7 +5,13 @@
   };
   systemd.user.services.niri.enableDefaultPath = false;
 
-  environment.systemPackages = with pkgs; [ xwayland-satellite ];
+  environment.systemPackages = with pkgs; [
+    xwayland-satellite
+    pkgs.labwc
+    (pkgs.writeShellScriptBin "steam-labwc" ''
+      exec ${pkgs.labwc}/bin/labwc -S steam
+    '')
+  ];
   xdg.portal = {
     enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
