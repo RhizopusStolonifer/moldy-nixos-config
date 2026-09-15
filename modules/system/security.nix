07a889e0780ead204266e4b26ebeadcb8bc58f8a
Rhizopus Stolonifer <codeberg.pension282@passmail.net>
2026-09-15 01:41:02 -0700

    add plover

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -27,5 +27,7 @@
 
     # tell ModemManager to ignore the Moonlander's virtual serial port
     SUBSYSTEM=="tty", ATTRS{idVendor}=="3297", ENV{ID_MM_DEVICE_IGNORE}="1"
+
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="3297", ATTRS{idProduct}=="1969", ENV{ID_USB_INTERFACE_NUM}=="02", SYMLINK+="moonlander-steno", MODE:="0660", GROUP="dialout"
   '';
 }
