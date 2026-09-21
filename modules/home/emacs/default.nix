41ce4d2cfd0f576ec481a1adf857563f1ddac238
Rhizopus Stolonifer <codeberg.pension282@passmail.net>
2026-09-21 07:12:29 -0700

    Fix emacs ob-hledger

--- modules/home/emacs/default.nix
+++ modules/home/emacs/default.nix
@@ -1,4 +1,10 @@
-{ config, lib, pkgs, inputs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 {
   programs.emacs = {
     enable = true;
@@ -6,17 +12,18 @@
   };
 
   home.activation.installDoomEmacs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-  if [ ! -d "${config.home.homeDirectory}/.config/emacs" ]; then
-    $DRY_RUN_CMD cp -r ${inputs.doom-emacs} "${config.home.homeDirectory}/.config/emacs"
-    $DRY_RUN_CMD chmod -R u+w "${config.home.homeDirectory}/.config/emacs"
-  fi
+    if [ ! -d "${config.home.homeDirectory}/.config/emacs" ]; then
+      $DRY_RUN_CMD cp -r ${inputs.doom-emacs} "${config.home.homeDirectory}/.config/emacs"
+      $DRY_RUN_CMD chmod -R u+w "${config.home.homeDirectory}/.config/emacs"
+    fi
 
-  if [ -x "${config.home.homeDirectory}/.config/emacs/bin/doom" ]; then
-    $DRY_RUN_CMD "${config.home.homeDirectory}/.config/emacs/bin/doom" sync -e || true
-  fi
-'';
+    if [ -x "${config.home.homeDirectory}/.config/emacs/bin/doom" ]; then
+      $DRY_RUN_CMD "${config.home.homeDirectory}/.config/emacs/bin/doom" sync -e || true
+    fi
+  '';
 
   xdg.configFile."doom/init.el".source = ./doom.d/init.el;
   xdg.configFile."doom/config.el".source = ./doom.d/config.el;
   xdg.configFile."doom/packages.el".source = ./doom.d/packages.el;
+  # xdg.configFile."doom/ob-hledger.el".source = ./doom.d/ob-hledger.el;
 }
